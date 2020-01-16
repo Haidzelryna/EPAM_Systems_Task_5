75bc3b739fcae87686c9bf536cdc14bc182b3808
Irina Haidzel <scorpion_nova@tut.by>
2020-01-16 22:34:37 +0300

    ContactData

---
+++ Task5/Views/Contacts/ContactsData.cs
@@ -0,0 +1,53 @@
+﻿using DevExtreme.MVC.Demos.Models.DataGrid;
+using System;
+using System.Collections.Generic;
+using Task5.Models;
+
+namespace Task5.View.Sales
+{
+    public partial class ContactData
+    {
+        public static readonly IEnumerable<Contact> DataGridSales = new[] {
+            new Contact {
+                Id = new Guid(),
+                FirstName = "John",
+                MiddleName = "Johnich",
+                LastName = "Heart",
+                Phone = "(213) 555-9392",
+                Email = "jheart@dx-email.com"
+            },
+            new Contact {
+                Id = new Guid(),
+                FirstName = "Olivia",
+                MiddleName = "Johnich",
+                LastName = "Peyton",
+                Phone = "(310) 555-2728",
+                Email = "oliviap@dx-email.com"
+            },
+            new Contact {
+                Id = new Guid(),
+                FirstName = "Robert",
+                MiddleName = "Johnich",
+                LastName = "Reagan",
+                Phone = "(818) 555-2387",
+                Email = "robertr@dx-email.com"                    
+            },
+            new Contact {
+                Id = new Guid(),
+                FirstName = "Greta",
+                MiddleName = "Johnich",
+                LastName = "Sims",
+                Phone = "(818) 555-6546",
+                Email = "gretas@dx-email.com"
+            },
+            new Contact {
+                Id = new Guid(),
+                FirstName = "Brett",
+                MiddleName = "Johnich",
+                LastName = "Wade",
+                Phone = "(626) 555-0358",
+                Email = "brettw@dx-email.com"
+            }
+        };
+    }
+}
