553ef5bdc8d6c047a7c0a2bc775b3fe2e0112bff
Irina Haidzel <scorpion_nova@tut.by>
2020-01-16 16:25:41 +0300

    Html.DevExtreme().DataGrid  Start

---
+++ Task5/Views/Sales/DataGridController.cs
@@ -0,0 +1,17 @@
+﻿using DevExtreme.MVC.Demos.Models;
+using DevExtreme.MVC.Demos.Models.DataGrid;
+using DevExtreme.MVC.Demos.Models.SampleData;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Task5.Views.Sales
+{
+    public class DataGridController : Controller
+    {
+        public ActionResult ColumnCustomization()
+        {
+            return View(SampleData.DataGridEmployees.Take(10));
+        }
+    }
+}
