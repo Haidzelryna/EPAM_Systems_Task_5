1038b09b6c1cdce816f9b6a557d0b826e04d7816
Irina Haidzel <scorpion_nova@tut.by>
2020-01-17 06:44:34 +0300

    Code

--- Task5/Controllers/ContactsController.cs
+++ Task5/Controllers/ContactsController.cs
@@ -34,9 +34,7 @@ namespace Task5.Controllers
         {
             //return View(await db.Contact.ToListAsync());
 
-            var i = (IEnumerable<Contact>)db.Contact.ToList();
-
-            return View(i);
+            return View((IEnumerable<Contact>)db.Contact.ToList());
         }
 
         // GET: Contacts/Details/5
@@ -60,80 +58,80 @@ namespace Task5.Controllers
         //    return View();
         //}
 
-        //// POST: Contacts/Create
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> Create([Bind(Include = "Id,FirstName,MiddleName,LastName,Phone,Email")] Contact contact)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        contact.Id = Guid.NewGuid();
-        //        db.Contact.Add(contact);
-        //        await db.SaveChangesAsync();
-        //        return RedirectToAction("Index");
-        //    }
+        // POST: Contacts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,FirstName,MiddleName,LastName,Phone,Email")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                contact.Id = Guid.NewGuid();
+                db.Contact.Add(contact);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
 
-        //    return View(contact);
-        //}
+            return View(contact);
+        }
 
-        //// GET: Contacts/Edit/5
-        //public async Task<ActionResult> Edit(Guid? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Contact contact = await db.Contact.FindAsync(id);
-        //    if (contact == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(contact);
-        //}
+        // GET: Contacts/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = await db.Contact.FindAsync(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
 
-        //// POST: Contacts/Edit/5
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> Edit([Bind(Include = "Id,FirstName,MiddleName,LastName,Phone,Email")] Contact contact)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(contact).State = EntityState.Modified;
-        //        await db.SaveChangesAsync();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(contact);
-        //}
+        // POST: Contacts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,FirstName,MiddleName,LastName,Phone,Email")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
 
-        //// GET: Contacts/Delete/5
-        //public async Task<ActionResult> Delete(Guid? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Contact contact = await db.Contact.FindAsync(id);
-        //    if (contact == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(contact);
-        //}
+        // GET: Contacts/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = await db.Contact.FindAsync(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
 
-        //// POST: Contacts/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> DeleteConfirmed(Guid id)
-        //{
-        //    Contact contact = await db.Contact.FindAsync(id);
-        //    db.Contact.Remove(contact);
-        //    await db.SaveChangesAsync();
-        //    return RedirectToAction("Index");
-        //}
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            Contact contact = await db.Contact.FindAsync(id);
+            db.Contact.Remove(contact);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
 
         //protected override void Dispose(bool disposing)
         //{
