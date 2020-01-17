74a68c6204c3ba836ef4ab62c861648fc27acb3d
Irina Haidzel <scorpion_nova@tut.by>
2020-01-18 02:34:48 +0300

    LAST BEFORE CHANGES

--- Task5/App_Start/RouteConfig.cs
+++ Task5/App_Start/RouteConfig.cs
@@ -16,8 +16,10 @@ namespace Task5
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+
+            routes.MapRoute("SalesWebApi", "api/SalesWebApi/{action}",
+                defaults: new { action = "Get" });
         }
     }
 }
