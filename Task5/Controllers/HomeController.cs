f7ba7cd9c04a7886565765dcc9022983ca670cfd
Irina Haidzel <scorpion_nova@tut.by>
2020-01-20 04:46:30 +0300

    Home Controller

--- Task5/Controllers/HomeController.cs
+++ Task5/Controllers/HomeController.cs
@@ -8,39 +8,39 @@ namespace Task5.Controllers
 {
     public class HomeController : Controller
     {
-        static List<Phone> data = new List<Phone>
-        {
-            new Phone { Id = Guid.NewGuid().ToString(), Name="iPhone 7", Price=52000 },
-            new Phone { Id = Guid.NewGuid().ToString(), Name="Samsung Galaxy S7", Price=42000 },
-        };
+        //static List<Phone> data = new List<Phone>
+        //{
+        //    new Phone { Id = Guid.NewGuid().ToString(), Name="iPhone 7", Price=52000 },
+        //    new Phone { Id = Guid.NewGuid().ToString(), Name="Samsung Galaxy S7", Price=42000 },
+        //};
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult GetPhones()
-        {
-            return Json(data, JsonRequestBehavior.AllowGet);
-        }
+        //public ActionResult GetPhones()
+        //{
+        //    return Json(data, JsonRequestBehavior.AllowGet);
+        //}
 
-        [HttpPost]
-        public ActionResult AddPhone(Phone phone)
-        {
-            phone.Id = Guid.NewGuid().ToString();
-            data.Add(phone);
-            return Json(phone);
-        }
+        //[HttpPost]
+        //public ActionResult AddPhone(Phone phone)
+        //{
+        //    phone.Id = Guid.NewGuid().ToString();
+        //    data.Add(phone);
+        //    return Json(phone);
+        //}
 
-        [HttpDelete]
-        public ActionResult DeletePhone(string id)
-        {
-            Phone phone = data.FirstOrDefault(x => x.Id == id);
-            if (phone != null)
-            {
-                data.Remove(phone);
-                return Json(phone);
-            }
-            return HttpNotFound();
-        }
+        //[HttpDelete]
+        //public ActionResult DeletePhone(string id)
+        //{
+        //    Phone phone = data.FirstOrDefault(x => x.Id == id);
+        //    if (phone != null)
+        //    {
+        //        data.Remove(phone);
+        //        return Json(phone);
+        //    }
+        //    return HttpNotFound();
+        //}
     }
 }
 //В качестве источника данных здесь используется массив объектов Phone, но при необходимости его можно заменить на контекст EF для работы с базо
