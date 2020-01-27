ef76751fd4f5c8822331f1fa38c4abbb5676efe4
Irina Haidzel <scorpion_nova@tut.by>
2020-01-27 03:09:31 +0300

    curent user get

--- Task5/Controllers/SalesController.cs
+++ Task5/Controllers/SalesController.cs
@@ -1,5 +1,26 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Task5.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Task5.Models;
+using System.Threading;
 
 namespace Task5.Controllers
 {
@@ -7,6 +28,13 @@ namespace Task5.Controllers
     {
         public async Task<ActionResult> Index()
         {
+            //var user = HttpContext.GetOwinContext().Get<ApplicationUserManager>();
+            //var roles = user.GetRoles(user.id);
+
+            var store = new UserStore<ApplicationUser>(new ApplicationDbContext());
+            var userManager = new UserManager<ApplicationUser>(store);
+            ApplicationUser user = userManager.FindByNameAsync(User.Identity.Name).Result;
+
             return View();
         }
     }
